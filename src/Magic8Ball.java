da51096abf6fc15af7bdb6f0adf22ae9e06e055a
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-10-05 08:01:05 -0400

    added random number feature

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -7,56 +7,59 @@ public class Magic8Ball {
         Scanner keyboard;
         int testing;
         String question;
+        int pickednumber;
 
 
 
         keyboard=new Scanner(System.in);
 
+
+        pickednumber=(int)(Math.random()*100);
+
         System.out.println("ask the magic 8 ball a question  ");
         question=keyboard.nextLine();
-        System.out.println("Write a number from 0-100 ");
-        testing=keyboard.nextInt();
-        if (testing<11) {
-            System.out.println("you're the man ");
+
+        if (pickednumber<11) {
+            System.out.println("not happening");
         }
-        else if  (testing<21) {
+        else if  (pickednumber<21) {
 
             System.out.println("looking good");
         }
 
-        else if (testing<31) {
+        else if (pickednumber<31) {
 
-            System.out.println("not happening ");
+            System.out.println("yeah probably ");
 
         }
-        else if (testing<41) {
+        else if (pickednumber<41) {
 
             System.out.println("yes sir");
         }
-        else if (testing<51) {
+        else if (pickednumber<51) {
 
             System.out.println("Luck is on your side");
 
         }
 
-        else if (testing<61) {
+        else if (pickednumber<61) {
             System.out.println("you hit the jackpot");
         }
 
 
-        else if (testing<71) {
+        else if (pickednumber<71) {
             System.out.println("doesn't look good ");
         }
 
-        else if (testing<81) {
+        else if (pickednumber<81) {
             System.out.println("never ");
         }
 
-        else if (testing<91) {
+        else if (pickednumber<91) {
             System.out.println("error");
         }
 
-        else if (testing<101) {
+        else if (pickednumber<101) {
             System.out.println("the future doesn't look bright for you");
         }
 
